ViaVersion/ViaFabricPlus

commit a4794e3e483772ba195af3cad4d8c9f94b3e5672

Added and fixed tons of clientside fixes

src/main/java/de/florianmichael/viafabricplus/injection/mixin/base/integration/MixinConnectScreen_1.java
@@ -19,30 +19,71 @@
 
 package de.florianmichael.viafabricplus.injection.mixin.base.integration;
 
+import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viafabricplus.ViaFabricPlus;
-import de.florianmichael.viafabricplus.protocolhack.provider.vialegacy.ViaFabricPlusClassicMPPassProvider;
-import de.florianmichael.viafabricplus.settings.impl.AuthenticationSettings;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.network.ClientConnection;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
+import de.florianmichael.viafabricplus.injection.access.IServerInfo;
+import de.florianmichael.viafabricplus.protocolhack.ProtocolHack;
+import de.florianmichael.viafabricplus.settings.impl.GeneralSettings;
+import net.lenni0451.mcping.MCPing;
+import net.minecraft.client.gui.screen.ConnectScreen;
+import net.minecraft.client.network.ServerInfo;
+import net.minecraft.client.session.Session;
+import net.minecraft.text.Text;
+import net.raphimc.vialoader.util.VersionEnum;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import java.net.InetSocketAddress;
 
 @Mixin(targets = "net.minecraft.client.gui.screen.ConnectScreen$1")
 public abstract class MixinConnectScreen_1 {
 
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/packet/Packet;)V"))
-    private void spoofUserName(ClientConnection instance, Packet<?> packet) {
-        if (AuthenticationSettings.global().setSessionNameToClassiCubeNameInServerList.getValue() && ViaFabricPlusClassicMPPassProvider.classiCubeMPPass != null) {
-            final var account = ViaFabricPlus.global().getSaveManager().getAccountsSave().getClassicubeAccount();
-            if (account != null) {
-                instance.send(new LoginHelloC2SPacket(account.username(), MinecraftClient.getInstance().getSession().getUuidOrNull()));
-                return;
-            }
+    @Shadow
+    @Final
+    ServerInfo field_40415;
+
+    @Shadow
+    @Final
+    ConnectScreen field_2416;
+
+    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/session/Session;getUsername()Ljava/lang/String;"))
+    private String useClassiCubeUsername(Session instance) {
+        final var account = ViaFabricPlus.global().getSaveManager().getAccountsSave().getClassicubeAccount();
+        if (account != null) {
+            return account.username();
         }
 
-        instance.send(packet);
+        return instance.getUsername();
     }
+
+    @SuppressWarnings("InvalidInjectorMethodSignature")
+    @Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;connect(Ljava/net/InetSocketAddress;ZLnet/minecraft/network/ClientConnection;)Lio/netty/channel/ChannelFuture;", shift = At.Shift.BEFORE), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void setServerInfo(CallbackInfo ci, InetSocketAddress inetSocketAddress) {
+        final VersionEnum serverVersion = ((IServerInfo) this.field_40415).viaFabricPlus$forcedVersion();
+        if (serverVersion != null) {
+            ProtocolHack.setTargetVersion(serverVersion);
+        } else if (GeneralSettings.global().autoDetectVersion.getValue()) {
+            this.field_2416.setStatus(Text.translatable("base.viafabricplus.detecting_server_version"));
+            MCPing
+                    .pingModern(-1)
+                    .address(inetSocketAddress.getHostString(), inetSocketAddress.getPort())
+                    .noResolve()
+                    .timeout(1000, 1000)
+                    .exceptionHandler(t -> {
+                    })
+                    .responseHandler(r -> {
+                        if (ProtocolVersion.isRegistered(r.version.protocol)) {
+                            ProtocolHack.setTargetVersion(VersionEnum.fromProtocolId(r.version.protocol));
+                        }
+                    })
+                    .getSync();
+        }
+    }
+
 }

src/main/java/de/florianmichael/viafabricplus/injection/mixin/fixes/minecraft/screen/MixinConnectScreen_1.java
@@ -19,6 +19,7 @@
 
 package de.florianmichael.viafabricplus.injection.mixin.fixes.minecraft.screen;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.viaversion.viaversion.api.connection.UserConnection;
 import com.viaversion.viaversion.api.minecraft.ProfileKey;
 import com.viaversion.viaversion.api.minecraft.signature.storage.ChatSession1_19_0;
@@ -27,20 +28,12 @@ import de.florianmichael.viafabricplus.ViaFabricPlus;
 import de.florianmichael.viafabricplus.injection.access.IClientConnection;
 import de.florianmichael.viafabricplus.injection.access.ILegacyKeySignatureStorage;
 import de.florianmichael.viafabricplus.protocolhack.ProtocolHack;
-import de.florianmichael.viafabricplus.protocolhack.provider.vialegacy.ViaFabricPlusClassicMPPassProvider;
-import de.florianmichael.viafabricplus.settings.impl.AuthenticationSettings;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.ConnectScreen;
 import net.minecraft.client.network.ServerAddress;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.encryption.PlayerPublicKey;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.c2s.login.LoginHelloC2SPacket;
-import net.raphimc.minecraftauth.MinecraftAuth;
-import net.raphimc.minecraftauth.util.MicrosoftConstants;
 import net.raphimc.viabedrock.protocol.storage.AuthChainData;
 import net.raphimc.vialoader.util.VersionEnum;
-import org.apache.http.impl.client.CloseableHttpClient;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -59,78 +52,58 @@ public abstract class MixinConnectScreen_1 {
     @Shadow
     ServerAddress field_33737;
 
-    @Final
-    @Shadow
-    ConnectScreen field_2416;
-
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/net/InetSocketAddress;getHostName()Ljava/lang/String;", ordinal = 0))
-    private String replaceAddress(InetSocketAddress instance) {
-        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_17) || ProtocolHack.getTargetVersion() == VersionEnum.bedrockLatest) {
+    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/net/InetSocketAddress;getHostName()Ljava/lang/String;", remap = false))
+    private String getRealAddress(InetSocketAddress instance) {
+        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_17)) {
             return field_33737.getAddress();
         }
 
         return instance.getHostName();
     }
 
-    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/net/InetSocketAddress;getPort()I"))
-    private int replacePort(InetSocketAddress instance) {
-        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_17) || ProtocolHack.getTargetVersion() == VersionEnum.bedrockLatest) {
+    @Redirect(method = "run", at = @At(value = "INVOKE", target = "Ljava/net/InetSocketAddress;getPort()I", remap = false))
+    private int getRealPort(InetSocketAddress instance) {
+        if (ProtocolHack.getTargetVersion().isOlderThanOrEqualTo(VersionEnum.r1_17)) {
             return field_33737.getPort();
         }
 
         return instance.getPort();
     }
 
-    @Inject(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/ClientConnection;send(Lnet/minecraft/network/packet/Packet;)V", shift = At.Shift.BEFORE))
-    private void setupConnectionSessions(CallbackInfo ci) {
-        final ClientConnection connection = field_2416.connection;
-        if (connection == null || connection.channel == null) return;
-
-        final UserConnection userConnection = ((IClientConnection) connection).viaFabricPlus$getUserConnection();
+    @Inject(method = "run", at = @At(value = "INVOKE", target = "Lio/netty/channel/ChannelFuture;syncUninterruptibly()Lio/netty/channel/ChannelFuture;", shift = At.Shift.AFTER))
+    private void setupConnectionSessions(CallbackInfo ci, @Local ClientConnection clientConnection) {
+        final UserConnection userConnection = ((IClientConnection) clientConnection).viaFabricPlus$getUserConnection();
         if (userConnection == null) return;
 
         final VersionEnum targetVersion = VersionEnum.fromUserConnection(userConnection);
 
-        if (targetVersion == VersionEnum.bedrockLatest) {
-            var bedrockSession = ViaFabricPlus.global().getSaveManager().getAccountsSave().getBedrockAccount();
-            if (bedrockSession == null) return;
-
-            try (final CloseableHttpClient httpClient = MicrosoftConstants.createHttpClient()) {
-                bedrockSession = MinecraftAuth.BEDROCK_DEVICE_CODE_LOGIN.refresh(httpClient, bedrockSession);
-            } catch (Exception e) {
-                ViaFabricPlus.global().getLogger().error("Failed to refresh Bedrock chain data. Please re-login to Bedrock!", e);
-                return;
-            }
-
-            final var deviceId = bedrockSession.getMcChain().getXblXsts().getInitialXblSession().getXblDeviceToken().getId();
-            final var playFabId = bedrockSession.getPlayFabToken().getPlayFabId();
-            final var mcChain = bedrockSession.getMcChain();
-
-            userConnection.put(new AuthChainData(mcChain.getMojangJwt(), mcChain.getIdentityJwt(), mcChain.getPublicKey(), mcChain.getPrivateKey(), deviceId, playFabId));
-            return;
-        }
-
-        if (targetVersion.isOlderThan(VersionEnum.r1_19)) {
-            return; // This disables the chat session emulation for all versions <= 1.18.2
-        }
-        if (targetVersion.isOlderThanOrEqualTo(VersionEnum.r1_19_1tor1_19_2)) {
-            final var profile = MinecraftClient.getInstance().getProfileKeys().fetchKeyPair().join().orElse(null);
-            if (profile != null) {
-                final PlayerPublicKey.PublicKeyData publicKeyData = profile.publicKey().data();
+        if (targetVersion.isBetweenInclusive(VersionEnum.r1_19, VersionEnum.r1_19_1tor1_19_2)) {
+            final var keyPair = MinecraftClient.getInstance().getProfileKeys().fetchKeyPair().join().orElse(null);
+            if (keyPair != null) {
+                final PlayerPublicKey.PublicKeyData publicKeyData = keyPair.publicKey().data();
 
                 final UUID playerUuid = MinecraftClient.getInstance().getSession().getUuidOrNull();
 
-                userConnection.put(new ChatSession1_19_1(playerUuid, profile.privateKey(), new ProfileKey(publicKeyData.expiresAt().toEpochMilli(), publicKeyData.key().getEncoded(), publicKeyData.keySignature())));
+                userConnection.put(new ChatSession1_19_1(playerUuid, keyPair.privateKey(), new ProfileKey(publicKeyData.expiresAt().toEpochMilli(), publicKeyData.key().getEncoded(), publicKeyData.keySignature())));
                 if (targetVersion == VersionEnum.r1_19) {
                     final var legacyKey = ((ILegacyKeySignatureStorage) (Object) publicKeyData).viafabricplus$getLegacyPublicKeySignature();
                     if (legacyKey != null) {
-                        userConnection.put(new ChatSession1_19_0(playerUuid, profile.privateKey(), new ProfileKey(publicKeyData.expiresAt().toEpochMilli(), publicKeyData.key().getEncoded(), legacyKey)));
+                        userConnection.put(new ChatSession1_19_0(playerUuid, keyPair.privateKey(), new ProfileKey(publicKeyData.expiresAt().toEpochMilli(), publicKeyData.key().getEncoded(), legacyKey)));
                     } else {
                         ViaFabricPlus.global().getLogger().error("Failed to fetch legacy key, can't setup ChatSession");
                     }
                 }
             } else {
                 ViaFabricPlus.global().getLogger().error("Failed to fetch keyPair, can't setup ChatSession");
+            }
+        } else if (targetVersion == VersionEnum.bedrockLatest) {
+            var bedrockSession = ViaFabricPlus.global().getSaveManager().getAccountsSave().refreshAndGetBedrockAccount();
+            if (bedrockSession != null) {
+                final var deviceId = bedrockSession.getMcChain().getXblXsts().getInitialXblSession().getXblDeviceToken().getId();
+                final var playFabId = bedrockSession.getPlayFabToken().getPlayFabId();
+                final var mcChain = bedrockSession.getMcChain();
+
+                userConnection.put(new AuthChainData(mcChain.getMojangJwt(), mcChain.getIdentityJwt(), mcChain.getPublicKey(), mcChain.getPrivateKey(), deviceId, playFabId));
             }
         }
     }

Peaks2000/ViaFabricPlusPeaks

commit 08c74c4ab00c66681d26f4f05c8c4005ae2e95c9

Fixed bug introduced with https://github.com/ViaVersion/ViaFabricPlus/commit/a4794e3e483772ba195af3cad4d8c9f94b3e5672

src/main/java/de/florianmichael/viafabricplus/injection/mixin/base/integration/MixinConnectScreen_1.java
@@ -23,6 +23,8 @@ import com.viaversion.viaversion.api.protocol.version.ProtocolVersion;
 import de.florianmichael.viafabricplus.ViaFabricPlus;
 import de.florianmichael.viafabricplus.injection.access.IServerInfo;
 import de.florianmichael.viafabricplus.protocolhack.ProtocolHack;
+import de.florianmichael.viafabricplus.protocolhack.provider.vialegacy.ViaFabricPlusClassicMPPassProvider;
+import de.florianmichael.viafabricplus.settings.impl.AuthenticationSettings;
 import de.florianmichael.viafabricplus.settings.impl.GeneralSettings;
 import net.lenni0451.mcping.MCPing;
 import net.minecraft.client.gui.screen.ConnectScreen;
@@ -54,9 +56,11 @@ public abstract class MixinConnectScreen_1 {
 
     @Redirect(method = "run", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/session/Session;getUsername()Ljava/lang/String;"))
     private String useClassiCubeUsername(Session instance) {
-        final var account = ViaFabricPlus.global().getSaveManager().getAccountsSave().getClassicubeAccount();
-        if (account != null) {
-            return account.username();
+        if (AuthenticationSettings.global().setSessionNameToClassiCubeNameInServerList.getValue() && ViaFabricPlusClassicMPPassProvider.classiCubeMPPass != null) {
+            final var account = ViaFabricPlus.global().getSaveManager().getAccountsSave().getClassicubeAccount();
+            if (account != null) {
+                return account.username();
+            }
         }
 
         return instance.getUsername();